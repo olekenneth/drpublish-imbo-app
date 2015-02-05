718bbf8bd46da030476b206f45310b9beca2dbd3
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-05 14:46:56 +0100

    Remove description from edit panel

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
             <legend data-translate="SELECTED_IMAGE_TITLE"></legend>
 
             <img src="img/blank.gif" class="image-preview" alt="">
-            <p class="description">En eller annen tekst. Som wrapper seg sånn ca rundt det bildet over her. Håper det går greit.</p>
 
             <div class="action-buttons">
                 <button class="edit-image"><i class="fa fa-edit"></i> <span data-translate="SELECTED_IMAGE_EDIT_IMAGE"></span></button>
