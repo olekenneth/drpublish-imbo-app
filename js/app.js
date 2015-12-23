36de751977078edfca370b596cbda291f0530e14
Sindre Gulseth <sindre.gulseth@e24.no>
2015-12-23 11:42:22 +0100

    Sort metadata search by image created date

--- js/app.js
+++ js/app.js
@@ -512,7 +512,8 @@ define([
                     users: this.config.imbo.searchUsers,
                     metadata: true,
                     limit: query.limit(),
-                    page: query.page()
+                    page: query.page(),
+                    sort: ['metadata.date:desc']
                 }, onComplete);
             } else {
                 this.imbo.user(this.config.imbo.user).getImages(query, onComplete);
